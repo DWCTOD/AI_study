689de240fb0cc430eeaff906eef8bb73a37ba476
scutpanzhiwei@163.com <zwpan@datainsights.biz>
2019-07-04 09:10:30 +0800

    c

--- 合格的CV工程师/实战篇/opencv/（二）模糊图像检测/detect_blur.py
+++ 合格的CV工程师/实战篇/opencv/（二）模糊图像检测/detect_blur.py
@@ -1,17 +1,16 @@
-# USAGE
-# python detect_blur.py --images images
+# 用法：直接运行下面的操作即可
+# python3 detect_blur.py --images images
 
-# import the necessary packages
+# 导入需要的库
 from imutils import paths
 import argparse
 import cv2
 
 def variance_of_laplacian(image):
-	# compute the Laplacian of the image and then return the focus
-	# measure, which is simply the variance of the Laplacian
+	# 对图像进行拉普拉斯变换，并返回拉普拉斯的方差
 	return cv2.Laplacian(image, cv2.CV_64F).var()
 
-# construct the argument parse and parse the arguments
+# 设置需要输入的参数，-i 输入图像的路径（文件夹名）， -t 设置阈值的大小
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--images", required=True,
 	help="path to input directory of images")
@@ -19,22 +18,19 @@ ap.add_argument("-t", "--threshold", type=float, default=100.0,
 	help="focus measures that fall below this value will be considered 'blurry'")
 args = vars(ap.parse_args())
 
-# loop over the input images
+# 循环输入图片
 for imagePath in paths.list_images(args["images"]):
-	# load the image, convert it to grayscale, and compute the
-	# focus measure of the image using the Variance of Laplacian
-	# method
+	# 导入图像并转换为灰度图，计算我们关注的拉普拉斯变换后的方差
 	image = cv2.imread(imagePath)
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	fm = variance_of_laplacian(gray)
 	text = "Not Blurry"
 
-	# if the focus measure is less than the supplied threshold,
-	# then the image should be considered "blurry"
+	# 当方差小于阈值时，显示为模糊
 	if fm < args["threshold"]:
 		text = "Blurry"
 
-	# show the image
+	# 显示图片
 	cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
 		cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
 	cv2.imshow("Image", image)
